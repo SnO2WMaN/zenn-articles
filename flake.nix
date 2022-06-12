fa33d207c91382c6ec8a0e8f43e8ad5456356ddd
SnO2WMaN <me@sno2wman.net>
2022-06-13 07:23:17 +0900

    update env

--- flake.nix
+++ flake.nix
@@ -1,31 +1,34 @@
 {
   description = "SnO2WMaN's Articles for Zenn";
   inputs = {
-    nixpkgs = {
-      url = "github:NixOS/nixpkgs/nixos-unstable";
-    };
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-    };
-    devshell = {
-      url = "github:numtide/devshell";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    devshell.url = "github:numtide/devshell";
+
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
     };
   };
-  outputs = { self, nixpkgs, devshell, flake-utils, ... }@inputs:
+  outputs = {
+    self,
+    nixpkgs,
+    devshell,
+    flake-utils,
+    ...
+  } @ inputs:
     flake-utils.lib.eachDefaultSystem (
-      system: {
-        devShell =
-          let
-            pkgs = import nixpkgs {
-              inherit system;
-              overlays = [ devshell.overlay ];
-            };
-          in
-          pkgs.devshell.mkShell {
-            imports = [
-              (pkgs.devshell.importTOML ./devshell.toml)
-            ];
-          };
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [devshell.overlay];
+        };
+      in {
+        devShell = pkgs.devshell.mkShell {
+          imports = [
+            (pkgs.devshell.importTOML ./devshell.toml)
+          ];
+        };
       }
     );
 }
